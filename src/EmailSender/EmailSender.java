1f5d4fb448d358cf183cd15fd4af3c77de5c5d2f
Martin Tvarog <martin.tvarog@gmail.com>
2023-10-25 12:07:45 +0200

    update

---
+++ src/EmailSender/EmailSender.java
@@ -0,0 +1,50 @@
+package EmailSender;
+import java.net.*;
+import java.io.*;
+
+public class EmailSender {
+    Socket socket;
+    /*
+     * Constructor opens Socket to host/port. If the Socket throws an exception during opening,
+     * the exception is not handled in the constructor.
+     */
+    public EmailSender(String host, int port) throws UnknownHostException, IOException {
+        socket = new Socket("smtp.utb.cz", 25);
+    }
+    /*
+     * sends email from an email address to an email address with some subject and text.
+     * If the Socket throws an exception during sending, the exception is not handled by this method.
+     */
+    public void send(String from, String to, String subject, String text) throws IOException, InterruptedException {
+        try (var socket = new Socket("smtp.utb.cz", 25)) {
+            var in = socket.getInputStream();
+            var out = socket.getOutputStream();
+            out.write(("HELO pc1-52109\r\n" +
+                    "MAIL FROM: " + from + "\r\n" +
+                    "RCPT TO: " + to + "\r\n" +
+                    "DATA\r\n" +
+                    "Subject: " + subject + "\r\n" +
+                    text + "\r\n.\r\n"
+            ).getBytes());
+
+            out.flush();
+            var buffer = new byte[1024];
+            var count = in.read(buffer);
+
+            Thread.sleep(100);
+
+            System.out.write(buffer, 0, count);
+        }
+    }
+
+    /*
+     * sends QUIT and closes the socket
+     */
+    public void close() throws IOException {
+        var out = socket.getOutputStream();
+        out.write("QUIT\r\n".getBytes());
+        out.flush();
+        socket.close();
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,19 @@
 import java.io.IOException;
 import java.net.Socket;
 import java.util.List;
+import EmailSender.EmailSender;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        var arg = new String[]{"Hello", "World"};
-
         try {
-            try (var socket = new Socket("smtp.utb.cz", 25)) {
-                var in = socket.getInputStream();
-                var out = socket.getOutputStream();
-                out.write(("HELO pc1-52109\r\n" +
-                        "MAIL FROM: important@utb.cz\r\n" +
-                        "RCPT TO: m_tvarog@utb.cz \r\n" +
-                        "DATA\r\n" +
-                        "Subject: Dulezity mail ze studijniho\r\n" +
-                        "Tohle neni phishing haha\r\n.\r\n"
-                ).getBytes());
-
-                out.flush();
-
-                var buffer = new byte[1024];
-                var count = in.read(buffer);
-
-                System.out.write(buffer, 0, count);
-
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            var emailSender = new EmailSender("smtp.utb.cz", 25);
+            emailSender.send("m_tvarog@utb.cz", "m_tvarog@utb.cz", "Test", "Test");
+            emailSender.close();
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
